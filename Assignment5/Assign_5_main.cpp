7c00303387b3bcef11427ecadd1122544089290e
Alexander2116 <Aleksander2116k@gmail.com>
2023-03-20 23:59:05 +0000

    Update Assign_5_main.cpp

--- Assignment5/Assign_5_main.cpp
+++ Assignment5/Assign_5_main.cpp
@@ -12,7 +12,8 @@
 ** Classes: complex(double, double)
 **
 ** Comments: Not protected from division by 0
-**         :
+**         : Using uint32_t (or size_t) and putting negative value mean value = size(uint32_t)-value.
+				so user can put negative value but it will be turned into some large (2^32 - 1 - VALUE) positive value
 **
 ** Date:  17 March 2023
 **
@@ -29,14 +30,15 @@ class Matrix
 	// Friends
 	friend std::ostream & operator<<(std::ostream &os, const Matrix &mat);
 private:
-	int _rows{0}, _columns{0};
+	uint32_t _rows{0}, _columns{0};
 	double *_matrix_data{nullptr}; // it will be double[] 1D array/list
 public:
 	// Default constructor
 	Matrix()= default;
 	// Parameterized constructors
+	// Using (int) instead of (uint32_t) makes it easier to detect negative value (by the cost of memory)
 	Matrix(int m, int n){
-		if(m<1 && n<1){
+		if(m<1 || n<1){
 			std::cout<<"Cannot create the matrix"<<std::endl;
 			exit(1);
 		}
@@ -47,8 +49,9 @@ public:
 			_matrix_data[i] = 0;
 		}
 	}
+	// Using (int) instead of (uint32_t) makes it easier to detect negative value (by the cost of memory)
 	Matrix(double *p, int m, int n){
-		if(m<1 && n<1){
+		if(m<1 || n<1){
 			std::cout<<"Cannot create the matrix"<<std::endl;
 			exit(1);
 		}
@@ -73,7 +76,11 @@ public:
 	int get_cols() const{
 		return _columns;
 	} // Return number of columns
-	int index(int m, int n) const{
+	int index(uint32_t m, uint32_t n) const{
+		if(m>_rows || n>_columns){
+			std::cout << "Index out of range" << std::endl;
+			exit(1);
+		}
 		return (n-1)+(m-1)*_columns;
 	} // Return position in array of element (m,n)
 	
@@ -87,15 +94,58 @@ public:
 			std::cout << std::endl;
 		}
 	}
+
+	// Gives cropped matrix with removed m-th row and n-th column of the current matrix
+	Matrix crop(uint32_t m, uint32_t n){
+		if(m<1 || n<1 || m>_rows || n>_columns){
+			std::cout << "Crop(): row or column index must be greater than 0" << std::endl;
+			exit(1);
+		}
+		double *temp;
+		uint32_t new_m{_rows-1};
+		uint32_t new_n{_columns-1};
+
+		temp = new double[new_m*new_n];
+		// Temporary variables used in the loop
+		uint32_t curr_idx{0};
+		uint32_t temp_idx{0};
+		// m-th row starts at "(m-1)*columns" index and goes to "(m-1)*columns + columns" every 1 increment
+		// n-th columns starts at "(n-1)"" index and goes to "(rows-1)*columns + (n-1)" every "columns" increment
+		// It's easier to create new array than to copy and remove item from an existing array
+		for(int i = 0; i<_rows; i++){
+			for(int j = 0; j<_columns; j++){
+				curr_idx = i*_columns + j;
+				// Conditions for removed column/row as described above 
+				if(!((m-1)*_columns <= curr_idx && curr_idx < m*_columns) && !(curr_idx == (i*_columns+n-1))){
+					//std::cout << curr_idx;
+					temp[temp_idx] = _matrix_data[curr_idx];
+					temp_idx++;
+				}
+			}
+		}
+		return Matrix(temp, new_m, new_n);
+	}
 	
 	// Determinant Expansion by Minors
 	double determinant(){
 		return 0;
 	}
-	
-	double& operator[](int m){
-		return _matrix_data[m];
-	}
+
+	// For easy return of M_(mn) item of the matrix M
+	double& operator()(uint32_t m, uint32_t n){
+		if(m>_rows || n>_columns){
+			std::cout << "Index out of range" << std::endl;
+			exit(1);
+		}
+		return _matrix_data[index(m,n)];}
+
+	// To return m-th element of the array
+	double& operator[](uint32_t m){
+		if(m>_rows*_columns-1){
+			std::cout << "Index out of range" << std::endl;
+			exit(1);
+		}
+		return _matrix_data[m];}
 	
 	Matrix operator+(const Matrix& m){
 		if(m._rows == _rows && m._columns == _columns){
@@ -108,6 +158,7 @@ public:
 		}
 		else{
 			// Cannot add these matrices
+			std::cout << "Matrices cannot be added together" << std::endl;
 			exit(1);
 		}
 	}
@@ -122,7 +173,7 @@ public:
 			return Matrix(temp, _rows, _columns);
 		}
 		else{
-			// Cannot add these matrices
+			std::cout << "Matrices cannot be subtracted together" << std::endl;
 			exit(1);
 		}
 	}
@@ -149,6 +200,7 @@ public:
 					// Do the rows*columns summation
 					for(int k = 0; k<m._columns; k++){
 						temp[i*m._columns+j] += _matrix_data[i*_columns + k] * m._matrix_data[j + k*m._columns];
+						// Could also use index to do that, but I like it more like this
 					}
 				}
 			}
@@ -172,7 +224,7 @@ public:
 			return result_matrix;
 		}
 		else{
-			std::cout << "Error" << std::endl;
+			std::cout << "Error while rising matrices to the power of "<< n << std::endl;
 			exit(1);
 		}
 		
@@ -197,10 +249,6 @@ Matrix operator*(double a, const Matrix& m){
 	Matrix temp;
 	return temp;
 }
-Matrix operator/(const Matrix& m, double a){
-	Matrix temp;
-	return temp;
-}
 //=== defined functions ===
 // Functions to overload: << and >> operator for complex
 
@@ -208,7 +256,6 @@ Matrix operator/(const Matrix& m, double a){
 
 //=== main ===
 int main(){
-    Matrix m = Matrix(3,3);
 	double p[9] = 
 		{1,1,1,
 		2,2,2,
@@ -217,7 +264,7 @@ int main(){
 	n.show();
 	Matrix mm;
 	//mm = n*n;
-	mm = n-n;
+	mm = n.crop(2,2);
 	mm.show();
 
     return 0;
