8ed384e4ebaf83da7d47f593c7e4f42c6de23055
Alexander2116 <Aleksander2116k@gmail.com>
2023-04-04 14:32:56 +0100

    Final?

--- Assignment6/Assign_6_main.cpp
+++ Assignment6/Assign_6_main.cpp
@@ -1,24 +1,18 @@
 /*============================================================
 ** UTF-8, Win32
 **
-** Assignment 5  (Deadline: 24/03/2023)
+** Assignment 5  (Deadline: 21/04/2023)
 ** Name:  Aleksander Kedziora
 ** Student ID:  10458000
 **
 **
-** Purpose: Creating a matrix with functionalities like calculating determinant, multiplying, cropping (removing a column and a row)
-**        : Creating overloading function for arithmetic operation AND ostream(<<), istream(>>)
+** Purpose: Use abstract classes and inheritance by defining 2D and 3D shape classes
 **
-** Classes: Matrix(double, double) / Matrix(array, double, double)
+** Includes: Shapes.hpp
 **
-** Comments : Not protected from division by 0
-**          : Using uint32_t (or size_t) and putting negative value means value = size(uint32_t)-value.
-				so user can put negative value but it will be turned into some large (2^32 - 1 - VALUE) positive value
-			: User can creat its own matrix
-			: Determinant is calculated using Expansion by Minors algorithm (a recursive alg.)
-			: Multiplication and Determinant calculations were tested and compered with https://matrix.reshish.com/determinant.php
+** Comments:
 **
-** Date:  22 March 2023
+** Date:  31 March 2023
 **
 ===========================================================*/
 
